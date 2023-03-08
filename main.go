e62b9c2e14decba10cd59803697b29acb5797f51
Jeanluca <jeanlucafp@gmail.com>
2023-03-08 08:37:11 -0300

    Update main.go

--- main.go
+++ main.go
@@ -2,8 +2,23 @@ package main
 
 import (
 	"fmt"
+	"time"
 )
 
+func task(name string) {
+	for i := 0; i < 10; i++ {
+		fmt.Println("teste", i)
+		time.Sleep(1000)
+	}
+}
+
 func main() {
-	fmt.Println(uuid)
+	canal := make(chan string)
+
+	go func() {
+		canal <- "teste comunicacao thread"
+	}()
+
+	msg := <-canal
+	fmt.Println(msg)
 }
